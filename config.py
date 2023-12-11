b24fec55e2ed8c12995c80184bd05d4cb3e79396
Rifqifdl <98653387+Rifqifdl@users.noreply.github.com>
2023-12-11 20:18:56 +0700

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ CLASSES = [ 'Screw' ]
 
 
 WEBRTC_CLIENT_SETTINGS = ClientSettings(
-        rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
+        rtc_configuration={"iceServers": [{"urls": ["stun.services.mozilla.com:3478"]}]},
         media_stream_constraints={"video": True, "audio": False},
     )
