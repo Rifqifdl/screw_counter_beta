7d57cb8d580ca756aa5cab523f17c036873610e3
Rifqifdl <98653387+Rifqifdl@users.noreply.github.com>
2023-12-11 20:36:01 +0700

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ CLASSES = [ 'Screw' ]
 
 
 WEBRTC_CLIENT_SETTINGS = ClientSettings(
-        rtc_configuration={"iceServers": [{"urls": ["freestun.net:3479"]}]},
+        rtc_configuration={"iceServers": [{"urls": ["turn:freestun.net:3479"], "username": "free", "credential": "free"}]},
         media_stream_constraints={"video": True, "audio": False},
     )
